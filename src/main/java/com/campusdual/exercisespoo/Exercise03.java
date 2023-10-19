8a5ae3b985d0c4bcb2fec4d9aca2fcc1bbeebb2b
carlosgarea <148324036+carlosgarea@users.noreply.github.com>
2023-10-19 10:31:44 +0200

    Add exercise 3

---
+++ src/main/java/com/campusdual/exercisespoo/Exercise03.java
@@ -0,0 +1,18 @@
+package com.campusdual.exercisespoo;
+
+public class Exercise03 {
+
+    public static String saludo(String nombre){
+        String mensaje = "Hola. Bienvenido, "+ nombre+"!";
+        return mensaje;
+    }
+
+    public static void fechaActual(String fecha){
+        System.out.println(fecha);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(saludo("Carlos"));
+        fechaActual("19/10/2023");
+    }
+}
